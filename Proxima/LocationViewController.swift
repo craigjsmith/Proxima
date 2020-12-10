77096566185ea19e15b1e98372335a7333b84333
Craig Smith <craigjsmith99@gmail.com>
2020-12-10 15:02:10 -0500

    Weather indicator added to location view

--- Proxima/LocationViewController.swift
+++ Proxima/LocationViewController.swift
@@ -11,10 +11,13 @@ import Parse
 class LocationViewController: UIViewController {
     
     var location: PFObject?
+    
+    let openWeatherAPI = "1aaa65db3029bc25e901f1b3db518f2c"
 
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var userLabel: UILabel!
     @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var weatherLabel: UILabel!
     @IBOutlet weak var categoryLabel: UILabel!
     @IBOutlet weak var pictureView: UIImageView!
     
@@ -22,11 +25,15 @@ class LocationViewController: UIViewController {
         super.viewDidLoad()
         
         if location != nil {
+            
+            // Set location name
             nameLabel.text = location!["name"] as! String
+            
+            // Set location description
             descriptionLabel.text = location!["description"] as! String
             
+            // Set owner
             let user = location?["author"] as! PFUser
-            // Loads the user if needed
             user.fetchIfNeededInBackground { (user, error) in
                 if user != nil {
                     let userPF = user as! PFUser
@@ -37,8 +44,15 @@ class LocationViewController: UIViewController {
                 
             }
             
+            // Set categories
             let categories = location!["categories"] as? [String] ?? []
             categoryLabel.text = categories.joined(separator: ", ")
+            
+            // Set weather
+            let lat = location?["lat"] as! Double
+            let long = location?["long"] as! Double
+            
+            getWeather(lat:lat, long:long)
             
         }
         
@@ -47,6 +61,56 @@ class LocationViewController: UIViewController {
         }
         
         // Do any additional setup after loading the view.
+    }
+    
+    func getWeather(lat: Double, long: Double) {
+        let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?lat=" + String(lat) + "&lon=" + String(long) + "&units=imperial" + "&appid=" + openWeatherAPI)!
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        let task = session.dataTask(with: request) { (data, response, error) in
+           // This will run when the network request returns
+           if let error = error {
+              print(error.localizedDescription)
+           } else if let data = data {
+                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+            
+                let mainJSON = dataDictionary["main"] as! [String: Any]
+                let temp = mainJSON["temp"] as! Double
+            
+                let weatherJSON = dataDictionary["weather"] as! [[String: Any]]
+                let subWeatherJSON = weatherJSON.first!
+                let condition = subWeatherJSON["main"] as! String
+            
+                print(condition)
+            
+                var emoji = ""
+                if(condition == "Thunderstorm") {
+                    emoji = "⚡️"
+                }
+                else if(condition == "Drizzle") {
+                    emoji = "☔️"
+                }
+                else if(condition == "Rain") {
+                    emoji = "☔️"
+                }
+                else if(condition == "Snow") {
+                    emoji = "🌨"
+                }
+                else if(condition == "Clear") {
+                    emoji = "☀️"
+                }
+                else if(condition == "Clouds") {
+                    emoji = "☁️"
+                }
+                else if(condition == "Drizzle") {
+                    emoji = "🌦"
+                }
+            
+                self.weatherLabel.text = emoji + String(Int(temp)) + "°"
+ 
+           }
+        }
+        task.resume()
     }
     
 
