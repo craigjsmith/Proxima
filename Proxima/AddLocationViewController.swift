98c0df9f7425898b09892e0336a39e0432910d35
Emmanuel Bangura <emmanuelbangurahsa@gmail.com>
2020-12-10 18:18:02 -0500

    Location reading and image upload updates.

--- Proxima/AddLocationViewController.swift
+++ Proxima/AddLocationViewController.swift
@@ -4,11 +4,13 @@
 //
 //  Created by Craig Smith on 11/30/20.
 //
-
 import UIKit
 import Parse
+import AlamofireImage
+import MapKit
 
-class AddLocationViewController: UIViewController, UITextFieldDelegate {
+
+class AddLocationViewController: UIViewController, UITextFieldDelegate,UIImagePickerControllerDelegate, UINavigationControllerDelegate, CLLocationManagerDelegate {
     
     @IBOutlet weak var locationName: UITextField!
     @IBOutlet weak var descriptionName: UITextField!
@@ -17,12 +19,22 @@ class AddLocationViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var urbanCheck: UISwitch!
     @IBOutlet weak var historicalCheck: UISwitch!
     @IBOutlet weak var photoCheck: UISwitch!
+   
+    var locationManager: CLLocationManager?
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         locationName.delegate = self
         descriptionName.delegate = self
+        
+        locationManager = CLLocationManager()
+        locationManager?.delegate = self
+        locationManager?.requestAlwaysAuthorization()
+        locationManager?.requestWhenInUseAuthorization()
+        locationManager?.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager?.distanceFilter = kCLDistanceFilterNone
+        locationManager?.startUpdatingLocation()
     }
     
     @IBAction func onSubmit(_ sender: Any) {
@@ -94,6 +106,40 @@ class AddLocationViewController: UIViewController, UITextFieldDelegate {
         }
         return false
     }
+    
+    @IBAction func onImageButton(_ sender: Any) {
+        
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.allowsEditing = true
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera){
+            picker.sourceType = .photoLibrary
+            
+        } else {
+            picker.sourceType = .photoLibrary
+        }
+        
+        present(picker, animated: true, completion: nil)
+        
+        let image = PFObject(className: "Image")
+    
+       /* let imageData = photoCheck.image!.pngData()
+        let file = PFFileObject(data: imageData!)
+        
+        image["Image"] = file
+        
+        image.saveInBackground {(success, error) in
+            if success {
+                self.dismiss(animated: true, completion: nil)
+                print("saved!")
+            } else {
+                print("error!")
+            }
+        }*/
+    }
+    
+    
     /*
     // MARK: - Navigation
 
