f6a05247a3d9d42051dc8c79592539b20500b74d
Craig Smith <craig@craigsmith.dev>
2022-08-25 14:21:25 -0400

    full screen image wip

--- Proxima/Location/LocationViewController.swift
+++ Proxima/Location/LocationViewController.swift
@@ -321,6 +321,26 @@ class LocationViewController: UIViewController, CLLocationManagerDelegate {
         self.addLocationButton.isEnabled = true
         addLocationButton.backgroundColor = UIColor.systemBlue
     }
+    
+    @IBAction func imageTapped(_ sender: UITapGestureRecognizer) {
+        let imageView = sender.view as! UIImageView
+        let newImageView = UIImageView(image: imageView.image)
+        newImageView.frame = UIScreen.main.bounds
+        newImageView.backgroundColor = .black
+        newImageView.contentMode = .scaleAspectFit
+        newImageView.isUserInteractionEnabled = true
+        let tap = UITapGestureRecognizer(target: self, action: #selector(dismissFullscreenImage))
+        newImageView.addGestureRecognizer(tap)
+        self.view.addSubview(newImageView)
+        self.navigationController?.isNavigationBarHidden = true
+        self.tabBarController?.tabBar.isHidden = true
+    }
+
+    @objc func dismissFullscreenImage(_ sender: UITapGestureRecognizer) {
+        self.navigationController?.isNavigationBarHidden = false
+        self.tabBarController?.tabBar.isHidden = false
+        sender.view?.removeFromSuperview()
+    }
 
     /*
     // MARK: - Navigation
